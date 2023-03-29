3d1febdbd9055dad7967019ffdbc0d9202c407e1
christian harrison <christianh@live.co.uk>
2023-03-29 22:10:15 +0100

    Removes bloat, prepares for pypi

--- app.py
+++
@@ -1,9 +0,0 @@
-from utils.twitter_scraper import TwitterScraper
-
-today_scraper = TwitterScraper(filter_links=True, filter_replies=True)
-
-tweets_df = today_scraper.run_scraper()
-
-
-tweets_df.to_csv('test2.csv')
-

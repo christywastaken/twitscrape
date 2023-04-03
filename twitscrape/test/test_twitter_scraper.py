51b345e0dd2109de108024f87e2b51eee8204f9a
christian harrison <christianh@live.co.uk>
2023-04-03 16:31:19 +0100

    Cleans project

--- twitscrape/test/test_twitter_scraper.py
+++
@@ -1,9 +0,0 @@
-import unittest
-from geolocation_twitter_scraper.twitter_scraper import TwitterGeolocationScraper
-
-
-test_scraper = TwitterGeolocationScraper(filter_replies=True, filter_links=True)
-tweet_df = test_scraper.run()
-
-print(tweet_df)
-unittest.main()
--- twitscrape/twitscrape/test.py
+++
@@ -1,9 +0,0 @@
-import unittest
-
-import twitter_scraper
-
-test_scraper = twitter_scraper.TwitterGeolocationScraper(is_headless=True, filter_replies=True)
-test_df = test_scraper.run()
-print(test_df.memory_usage(deep=True).sum())
-test_df.to_csv('test.csv', index=False)
-

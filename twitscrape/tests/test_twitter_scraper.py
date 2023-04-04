c2ae0a4bb6955030575260baafe56013e67dc4c4
christian harrison <christianh@live.co.uk>
2023-04-04 16:35:35 +0100

    Refactor tab spacing

--- twitscrape/tests/test_twitter_scraper.py
+++ twitscrape/tests/test_twitter_scraper.py
@@ -1,5 +1,8 @@
 from twitscrape.twitter_scraper import TwitterGeolocationScraper
 
-test_scraper = TwitterGeolocationScraper(filter_replies=True)
+test_scraper = TwitterGeolocationScraper(start_date='2022-06-20', end_date='2022-06-28', filter_links=True, filter_replies=True, is_headless=True)
+
 test_df = test_scraper.run()
 test_df.to_csv('test.csv')
+
+#TODO: Learn how to properly utilise pytest, rather than saving a csv of output.
